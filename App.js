712113cb279b0f5902a20afc50cfe192768f458d
Matryoshka.es <eugenia.ulko@gmail.com>
2021-10-04 19:34:14 +0200

    Add users

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import Group from './components/Group';
 export default function App() {
   return (
     <Container>
-      <Group title='14 de octubre' 
+      <Group 
+      title='14 de octubre' 
       items={[
         {
         time: '15:40',
@@ -16,8 +17,23 @@ export default function App() {
           avatar: 'https://sun6-23.userapi.com/s/v1/if1/5WKkHv1KXnc8K2HikPNB-v-gmt3CTZ9u9VCmYfpjNtGy7QBGq-S69fUw6TLuPHwZ8PFrzZmd.jpg?size=100x100&quality=96&crop=0,41,719,719&ava=1',
 
         }
-      }
-      ]}></Group>
+      },
+      ]}
+      />
+       <Group 
+      title='15 de octubre' 
+      items={[
+        {
+        time: '12:40',
+        diagnosis: 'Dolor de hombro',
+        user: {
+          fullname: 'Lina Martinez',
+          avatar: 'https://sun6-22.userapi.com/s/v1/ig2/thidMeEse39qpARRVPlVyyw9BfgVR3a04OoA4SLCsIOWIe1vdUdd8ksrhCB1hv9FmqPo1nWg6C-UP6Qyen6uBrj4.jpg?size=100x100&quality=96&crop=809,612,807,807&ava=1',
+
+        }
+      },
+      ]}
+      />
     </Container>
   );
 }
